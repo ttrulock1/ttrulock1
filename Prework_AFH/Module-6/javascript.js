ffd72e64a1dd482a7408fa4172cf4b66bba7ba88
Todd Trulock <toddstrulock@gmail.com>
2021-08-17 16:14:26 -0500

    Add files via upload

---
+++ Prework_AFH/Module-6/javascript.js
@@ -0,0 +1,14 @@
+var div = document.getElementById("box");
+div.setAttribute("id", "resetDiv");
+
+function blue() {
+    div.setAttribute("id", "blueDiv");}
+
+function reset() {
+    div.setAttribute("id", "resetDiv");}
+
+function fade() {
+    div.setAttribute("id", "fadeDiv");}
+function grow() {
+      div.setAttribute("id", "growDiv")
+}        
